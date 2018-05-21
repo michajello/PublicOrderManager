7aec6a431420f24b6ed541a27a1738b692a8a705
Michał Grzesik <mgrzesik7@gmail.com>
2018-05-21 10:22:12 +0200

    Completed missing information about Order type

--- dbmodel/src/main/java/pl/edu/agh/tai/dbmodel/entity/OrderType.java
+++ dbmodel/src/main/java/pl/edu/agh/tai/dbmodel/entity/OrderType.java
@@ -1,13 +1,23 @@
 package pl.edu.agh.tai.dbmodel.entity;
 
+import lombok.Getter;
+
+@Getter
 public enum OrderType {
-    ANNOUNCEMENT_ABOUT_ORDER("ZP-400", 1, "Ogłoszenie o zamówieniu");
+    ANNOUNCEMENT_ABOUT_ORDER("ZP-400", 1, "Ogłoszenie o zamówieniu"),
+    SIMPLIFIED_ANNOUNCEMENT(null, 2, "Uproszczone ogłoszenie"),
+    AWARD_OF_THE_CONTRACT(null, 3, "Udzielenie zamówienia"),
+    ANNOUNCEMENT_OF_THE_COMPETITION(null, 4, "Ogłoszenie konkursu"),
+    ANNOUNCEMENT_OF_THE_COMPETITION_RESULTS(null, 5, "Ogłoszenie wyników konkursu"),
+    CHANGE_OF_THE_ANNOUNCEMENT(null, 6, "Zmiana ogłoszenia"),
+    INTENTION_TO_AWARD_A_CONTRACT(null, 7, "Zamiar zawarcia umowy");
 
-    private String symbol;
-    private int id;
-    private String name;
 
-    private OrderType(String symbol, int id, String name) {
+    private final String symbol;
+    private final int id;
+    private final String name;
+
+    OrderType(String symbol, int id, String name) {
         this.symbol = symbol;
         this.id = id;
         this.name = name;
