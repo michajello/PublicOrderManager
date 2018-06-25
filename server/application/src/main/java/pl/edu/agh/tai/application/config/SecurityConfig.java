a0f383f20541cc457b11fe37bb516271a8d4be12
Michał Grzesik <mgrzesik7@gmail.com>
2018-06-25 18:34:59 +0200

    Added rule which allow make request to /order url for all users, even not authenticated

--- server/application/src/main/java/pl/edu/agh/tai/application/config/SecurityConfig.java
+++ server/application/src/main/java/pl/edu/agh/tai/application/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import pl.edu.agh.tai.application.util.ApiConstants;
 
 @EnableWebSecurity
 @Configuration
@@ -12,6 +13,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
+                .antMatchers(ApiConstants.ORDERS + "/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .oauth2Login();
